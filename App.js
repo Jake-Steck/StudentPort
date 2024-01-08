b15a1efeeafae1e6638eb522eebff3a5d9a525d3
Jake Stecklow <jakestecklownj@gmail.com>
2024-01-08 18:15:35 -0500

    added navigation stacks

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import OpenScreen from './pages/openScreen';
+import SignIn from './pages/signIn'
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="OpenScreen"
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="OpenScreen" component={OpenScreen} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
