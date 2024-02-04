274eb071dc3f6f6bee8b8d2d7af6942beb8a6917
Jake Stecklow <jakestecklownj@gmail.com>
2024-02-04 16:31:55 -0500

    profile ui

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
-import { useFonts, Poppins_700Bold, Poppins_300Light, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useFonts, Poppins_700Bold, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Profile({ route }) {
     const { userInfo } = route.params;
-    console.log(userInfo);
+
     const [fontsLoaded] = useFonts({
         Poppins_700Bold,
-        Poppins_300Light,
-        Poppins_600SemiBold,
-
+        Poppins_400Regular,
     });
 
     if (!fontsLoaded) {
@@ -18,7 +17,17 @@ export default function Profile({ route }) {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>{userInfo?.name}</Text>
+            <View style={styles.headerContainer}>
+                <View style={styles.circle} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.nameText}>{userInfo?.name}</Text>
+                    <Text style={styles.subText}>School</Text>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Classes</Text>
+                <AntDesign name="right" size={15} color="black" style={styles.icon} />
+            </TouchableOpacity>
         </View>
     );
 }
@@ -30,9 +39,49 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    text: {
-        fontFamily: 'Poppins_600SemiBold',
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    circle: {
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        backgroundColor: '#0e0e0e',
+        marginLeft: -50,
+    },
+    textContainer: {
+        marginLeft: 20,
+    },
+    nameText: {
+        fontFamily: 'Poppins_700Bold',
         fontSize: 25,
-        justifyContent: "center",
+    },
+    subText: {
+        fontFamily: 'Poppins_400Regular',
+        fontSize: 15,
+    },
+    buttonText: {
+        fontFamily: 'Poppins_400Regular',
+        fontSize: 15,
+        marginLeft: 10,
+        justifyContent: 'center',
+        textAlign: 'left',
+    },
+    button: {
+        marginTop: 50,
+        width: 335,
+        height: 48,
+        paddingHorizontal: 8,
+        borderWidth: 1,
+        borderColor: '#e3e3e3',
+        borderRadius: 12,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+    },
+    icon: {
+        position: 'absolute',
+        right: 15,
     },
 });
