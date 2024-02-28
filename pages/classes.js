198f754e50ee5ba7a0278dd237170c2435f7f673
Jake Stecklow <jakestecklownj@gmail.com>
2024-02-27 19:47:08 -0500

    display user's classes

--- pages/classes.js
+++ pages/classes.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
-import { useFonts, Poppins_700Bold, Poppins_300Light, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+// Inside classes.js
 
-export default function Profile({ route }) {
-    const { userInfo } = route.params;
-    console.log(userInfo);
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts, Poppins_700Bold, Poppins_300Light, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { FirestoreData } from '../components/firestoreData';
+
+export default function Classes() {
     const [fontsLoaded] = useFonts({
         Poppins_700Bold,
         Poppins_300Light,
         Poppins_600SemiBold,
-
     });
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userId = await FirestoreData.getUser();
+                const response = await FirestoreData.getClasses(userId);
+                setClasses(response);
+
+            } catch (error) {
+                console.error('Error fetching classes:', error);
+            }
+        };
+        fetchData();
+
+    }, []);
 
     if (!fontsLoaded) {
         return <Text>Loading...</Text>; // or any loading indicator
@@ -19,6 +35,9 @@ export default function Profile({ route }) {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Classes</Text>
+            {classes.map((item, index) => (
+                <Text key={index}>{item}</Text>
+            ))}
         </View>
     );
 }
