d97f707bf824cd507bce054c1c7bd0f7b58af975
Brandon <bmreitman@gmail.com>
2024-03-11 22:15:20 -0400

    Achievements

---
+++ pages/achievements.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList, TextInput } from 'react-native';
+import { useFonts, Poppins_700Bold, Poppins_300Light, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { getUser, removeFromPortfolio, getClubs } from '../components/firestoreData';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Achievements() {
+    const [fontsLoaded] = useFonts({
+        Poppins_700Bold,
+        Poppins_300Light,
+        Poppins_600SemiBold,
+    });
+    const [clubs, setClubs] = useState([]);
+    const [itemClicked, setItemClicked] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userId = await getUser();
+                const response = await getClubs(userId);
+                setClubs(response);
+
+            } catch (error) {
+                console.error('Error fetching sports:', error);
+            }
+        };
+        fetchData();
+
+    }, [itemClicked]);
+
+    if (!fontsLoaded) {
+        return <Text>Loading...</Text>; // or any loading indicator
+    }
+
+    const handleRemove = (item) => {
+        removeFromPortfolio(item, "clubs");
+        setItemClicked(!itemClicked);
+    }
+
+    const renderItem = ({ item }) => (
+        <TouchableOpacity style={styles.itemContainer} onPress={() => handleRemove(item)}>
+            <Text style={styles.itemText}>{item}</Text>
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.gap}/>
+            <Text style={styles.heading}>Achievements</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.iconText}>New Achievement</Text>
+                <TextInput
+                            style={[
+                                styles.input,
+                            ]}
+                            placeholder='Enter New Achievement'
+                    />
+                <Text style={styles.iconText}>Description / Details</Text>
+                <TextInput
+                            style={[
+                                styles.input,
+                            ]}
+                            placeholder='Enter Description'
+                    />
+            </View>
+            <TouchableOpacity style={styles.submitButton}>
+                <Text style={styles.submitButtonText}>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style={styles.back} onPress={() => navigation.push('Profile')}>Back</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f7f7f7',
+        padding: 20,
+    },
+    heading: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 25,
+        marginBottom: 0,
+        alignSelf: 'center',
+        top: 30,
+    },
+    itemContainer: {
+        borderRadius: 8,
+        padding: 15,
+        marginBottom: 10,
+        elevation: 2,
+        borderWidth: 2,
+        borderColor: 'black',
+        backgroundColor: 'white',
+    },
+    itemText: {
+        fontFamily: 'Poppins_300Light',
+        fontSize: 18,
+    },
+    inputContainer: {
+        borderRadius: 24,
+        width: 350,
+        height: 240,
+        backgroundColor: "lightgray",
+        alignSelf: 'center',
+        alignItems: 'left',
+        top: 40,
+    },
+    input: {
+        marginTop: 7,
+        width: 327,
+        height: 48,
+        paddingHorizontal: 8,
+        borderWidth: 1,
+        borderColor: '#ccccd1',
+        borderRadius: 12,
+        backgroundColor: 'rgba(252,252,252,0.86)',
+        color: '#222224',
+        fontSize: 14,
+        //fontFamily: 'Poppins_300Light',
+        lineHeight: 22,
+        marginLeft: 10,
+    },
+    iconText: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 16,
+        marginLeft: 15,
+        marginTop: 5,
+        textAlign: 'left',
+        color: 'black',
+    },
+    gap: {
+        marginTop: 15, 
+    },
+    submitButtonText: {
+        color: 'black',
+        fontFamily: 'Poppins_600SemiBold',
+    },
+    submitButton: {
+        marginTop: -23,
+        width: 150,
+        height: 48,
+        paddingHorizontal: 8,
+        borderWidth: 1,
+        borderColor: '#0e0e0e',
+        borderRadius: 12,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    back: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 18,
+        textAlign: 'center',
+        top: 40,
+        color: 'black',
+    },
+});
